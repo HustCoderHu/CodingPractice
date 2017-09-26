1ea76403df0a8f65245459ee84c2d38a7b407ae1
HustCoderHu <coderhzx@gmail.com>
2017-09-26 19:59:31 +0800

    Factory method modified

--- DesignPatterns/FactoryMethod/main.cpp
+++ DesignPatterns/FactoryMethod/main.cpp
@@ -1,82 +1,77 @@
 #include <iostream>
+#include <string>
+
+class Pet {
+public:
+//    char* brand;
+    virtual void sounds() = 0;
+    virtual ~Pet() = default;
+};
+
+class Cat : public Pet {
+public:
+    void sounds() override {
+        std::cout << "meow" << std::endl;
+    }
+
+    ~Cat(){}
+};
+
+class Dog : public Pet {
+public:
+    void sounds() override {
+        std::cout << "bark" << std::endl;
+    }
+
+    ~Dog(){}
+};
+
+class PetShop_base {
+public:
+//    Factory(const string &name) : pName(name) {}
+    virtual Pet* getPet() = 0;
+    virtual ~PetShop_base() = default;
+};
+
+class CatZone : public PetShop_base {
+public:
+//    Factory_A(const string &name) : pName(name) {}
+    Pet* getPet() override {
+        return new Cat();
+    }
+
+    ~CatZone(){}
+};
+
+class DogZone : public PetShop_base {
+public:
+//    Factory_B(const string &name) : pName(name) {}
+    Pet* getPet() override {
+        return new Dog();
+    }
+
+    ~DogZone(){}
+};
 
 using namespace std;
 
-class Product {
-public:
-
-    virtual const char* getName() = 0;
-    virtual ~Product() = default;
-private:
-    int productID;
-};
-
-class Product_A : public Product {
-public:
-    string productName;
-    Product_A(const string &name) : productName(name) {}
-    const char* getName() override {
-        return productName.c_str();
-    }
-
-    ~Product_A(){}
-};
-
-class Product_B : public Product {
-public:
-    string productName;
-    Product_B(const string &name) : productName(name) {}
-    const char* getName() override {
-        return productName.c_str();
-    }
-
-    ~Product_B(){}
-};
-
-class Factory {
-public:
-//    Factory(const string &name) : pName(name) {}
-    virtual Product* createProduct() = 0;
-};
-
-class Factory_A : public Factory {
-public:
-    string pName;
-    Factory_A(const string &name) : pName(name) {}
-    Product* createProduct() override {
-        return new Product_A(pName);
-    }
-
-    ~Factory_A(){}
-};
-
-class Factory_B : public Factory {
-public:
-    string pName;
-    Factory_B(const string &name) : pName(name) {}
-    Product* createProduct() override {
-        return new Product_B(pName);
-    }
-
-    ~Factory_B(){}
-};
-
 int main(int argc, char *argv[])
 {
-    Factory* fac = new Factory_B(string("bbb"));
-    Product* prob = fac->createProduct();
+    PetShop_base* dogzone = new DogZone();
+    Pet* dog = dogzone->getPet();
 
-    Factory* fac2 = new Factory_A(string("aaa"));
-    Product* proa = fac2->createProduct();
+    PetShop_base* catzone = new CatZone();
+    Pet* cat = catzone->getPet();
 
 ////    Product* proa = Factory_A("aaa").createProduct();
 
-    cout << proa->getName() << endl;
-    cout << prob->getName() << endl;
+    cat->sounds();
+    dog->sounds();
 
-    delete proa;
-    delete fac2;
-    delete prob;
-    delete fac;
+    delete cat;
+    delete catzone;
+    delete dog;
+    delete dogzone;
+
     return 0;
 }
