2ed30b4bf7e2283209eb6385f749c0fd6e70116c
HustCoderHu <coderhzx@gmail.com>
2018-01-02 20:12:19 +0800

    docker images :latest-gpu-py3

--- TensorFlow/mnist/dockerRun.sh
+++ TensorFlow/mnist/dockerRun.sh
@@ -1,18 +1,10 @@
-docker run -ti --rm \
---privileged=true \
---mount type=bind,source=/etc/localtime,target=/etc/localtime,readonly \
---mount type=bind,source=/etc/apt/sources.list,target=/etc/apt/sources.list,readonly \
---mount type=bind,source=/home/hzx,target=/home/hzx \
-tensorflow/tensorflow:latest-gpu bash
-
 docker run -ti --runtime=nvidia --rm \
 --mount type=bind,source=/etc/localtime,target=/etc/localtime,readonly \
 --mount type=bind,source=/etc/apt/sources.list,target=/etc/apt/sources.list,readonly \
 --mount type=bind,source=/home/hzx,target=/home/hzx \
-tensorflow/tensorflow:latest-gpu bash
+tensorflow/tensorflow:latest-gpu-py3 bash
 
 docker run -ti --rm --runtime=nvidia \
---privileged=true \
 --mount type=bind,source=/etc/localtime,target=/etc/localtime,readonly \
 --mount type=bind,source=/etc/apt,target=/etc/apt,readonly \
 --mount type=bind,source=/home/hzx,target=/home/hzx \
