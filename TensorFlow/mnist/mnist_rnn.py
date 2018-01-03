b4ad9e96b82f6f271538ae8f44116a9a2fabef85
HustCoderHu <coderhzx@gmail.com>
2018-01-04 01:00:38 +0800

    gru hidden=70 achive acc top1 97.7%

--- TensorFlow/mnist/mnist_rnn.py
+++ TensorFlow/mnist/mnist_rnn.py
@@ -13,7 +13,6 @@ import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
 
 # from tensorflow.contrib import rnn
-# import tensorflow.nn
 import tensorflow.contrib.rnn as rnn
 from tensorflow.python.client import timeline
 
@@ -31,46 +30,69 @@ def bias_variable(shape) :
   initial = tf.constant(0.1, shape=shape)
   return tf.Variable(initial)
   
-def RNN(inputs, n_hidden, n_classes) :
-  print(inputs.shape[0])
-  print(inputs.shape[1])
-  print(inputs.shape[2])
-  batch_size = int(inputs.shape[0])
-  state_size = int(inputs.shape[2])
+def RNN(rnntype, inputs, n_hidden, n_classes) :
+  # print(inputs.shape[0])
+  # print(inputs.shape[1])
+  # print(inputs.shape[2])
+  # batch_size = int(inputs.shape[0])
+  
+  print(inputs[0].shape[1])
+  state_size = int(inputs[0].shape[1])
+  
+  # for dynamic
+  # n_steps = int(inputs.shape[1])
+  # state_size = int(inputs.shape[2])
   # print(type(state_size))
   
-  # cell = rnn.LSTMCell(n_hidden)
-  cell = rnn.GRUCell(n_hidden)
-  initial_state = cell.zero_state(batch_size, tf.float32)
-  outputs, state = tf.nn.dynamic_rnn(cell, inputs, initial_state=initial_state)
+  if rnntype == "GRU" :
+    print("rnntype: " + rnntype)
+    cell = rnn.GRUCell(n_hidden)
+  else :
+    print("rnntype: " + rnntype)
+    cell = rnn.LSTMCell(n_hidden)
+
+  # initial_state = cell.zero_state(batch_size, tf.float32)
+  outputs, state = tf.nn.static_rnn(cell, inputs, dtype = tf.float32)
+  #initial_state=initial_state)
+  # outputs, state = tf.nn.dynamic_rnn(cell, inputs, initial_state=initial_state)
   output = outputs[-1] # final output
+  print(output.shape)
 
   with tf.name_scope('logits_vec') :
-    w = weight_variable([state_size, n_classes])
+    w = weight_variable([n_hidden, n_classes])
     b = weight_variable([n_classes])
  
-  output_mat = tf.reshape(output, [-1, state_size])
-  logits = tf.matmul(output_mat, w) + b
+  # output_mat = tf.reshape(output, [-1, state_size])
+  logits = tf.matmul(output, w) + b
     
   return logits
 
   
 def main(_):
+  rnntype = "GRU"
   # net param
   n_input = 28 # MNIST data input (img shape: 28*28)
   n_steps = 28 # timesteps
-  n_hidden = 20 #50 # hidden layer num of features
+  n_hidden = 70 #50 # hidden layer num of features
   n_classes = 10 # MNIST total classes (0-9 digits)
   
   # training param
   learning_rate = 1e-3
-  max_iter = 1000
-  test_interval = 5
-  train_batch_size = 50
-  display_step = 50
+  max_iter = 40000
+  train_batch_size = 500
+  display_step = 20
+  
+  # test_batch_size = 500
+  # test_iter = 20
+  test_interval = 60
+  
 
   mnist = input_data.read_data_sets(data_dir, one_hot=True, 
                                   source_url='http://yann.lecun.com/exdb/mnist/')
+  batch_imgs, batch_labels = mnist.train.next_batch(1)
+  # print(batch_imgs)
+  # print(batch_labels)
+
   # print("input: ")
   # print(mnist.train.images)
   # print("input shape: ")
@@ -82,12 +104,16 @@ def main(_):
   # pylab.imshow(im)
   # pylab.show()
   
-  inputs = tf.placeholder(tf.float32, [8, n_steps, n_input])
+  # inputs = tf.placeholder(tf.float32, [None, n_steps, n_input])
+  inputs = tf.placeholder(tf.float32, [None, n_steps*n_input])
   y = tf.placeholder(tf.float32, [None, n_classes])
   # print(inputs.shape)
   # print(inputs.shape[2])
+  inputs_list = []
+  inputs_list.append(inputs)
+  logits = RNN(rnntype, inputs_list, n_hidden, n_classes)
   
-  logits = RNN(inputs, n_hidden, n_classes)
+  # output = RNN(rnntype, inputs, n_hidden, n_classes)
 
   with tf.name_scope('accuracy'):
     correct_prediction = tf.equal(tf.argmax(logits, 1), tf.argmax(y, 1))
@@ -98,7 +124,7 @@ def main(_):
     cross_entropy = tf.nn.softmax_cross_entropy_with_logits(labels=y, logits=logits)
     cross_entropy = tf.reduce_mean(cross_entropy)
   
-  with tf.name_scope('adam_optimizer'):
+  with tf.name_scope('adam_optimizer') :
     train_step = tf.train.AdamOptimizer(learning_rate).minimize(cross_entropy)
   
   config = tf.ConfigProto()
@@ -106,54 +132,37 @@ def main(_):
   with tf.Session(config = config) as sess :
     sess.run(tf.global_variables_initializer())
     for i in range(max_iter) :
+      # test
+      if i % test_interval == 0 and i != 0 :
+        # batch_imgs, batch_labels = mnist.test.next_batch(test_batch_size)
+        batch_imgs, batch_labels = mnist.test.images, mnist.test.labels
+        test_accuracy = accuracy.eval(feed_dict={inputs: batch_imgs, y: batch_labels})
+        print("  step {} test accuracy {}".format(i, test_accuracy))
+      # train
       batch_imgs, batch_labels = mnist.train.next_batch(train_batch_size)
-      if i % 100 == 0 and i != 0 :
-        train_accuracy = accuracy.eval(feed_dict={x: batch_imgs, y: batch_labels})
-        print('step %d, training accuracy %g' % (i, train_accuracy))
-      train_step.run(feed_dict={x: batch_imgs, y: batch_labels})
-      
-      if 
-  
+      if i % display_step == 0 and i != 0 :
+        x_entropy = cross_entropy.eval(feed_dict={inputs: batch_imgs, y: batch_labels})
+        print("x_entropy: {}".format(x_entropy))
+      else :
+        train_step.run(feed_dict={inputs: batch_imgs, y: batch_labels})
+    
   return
-  
-  # cell = rnn.BasicLSTMCell(n_hidden, forget_bias=1.0)
-  # cell = rnn.BasicLSTMCell(n_hidden)
-  # cell = rnn.LSTMCell(n_hidden)
-  cell = rnn.GRUCell(n_hidden)
-  # print(cell.state_size)
-  initial_state = cell.zero_state(train_batch_size, tf.float32)
-  # inputs = tf.placeholder(tf.float32, [time_steps, train_batch_size, n_input])
-  # outputs, states = rnn.static_rnn(cell, inputs, dtype=tf.float32)
-  
-  # train_batch_size
-  num_steps = 28
-  features = n_input = 28
-  
-  # outputs, states = rnn.
-  # print(initial_state)
-  print(inputs)
-  # print(h1)
-  print(outputs)
-  
-  number_of_layers = 3
-  stacked_lstm = rnn.MultiRNNCell([rnn.LSTMCell(n_hidden) for _ in range(number_of_layers)])
-  
-  initial_state = state = stacked_lstm.zero_state(train_batch_size, tf.float32)
-  return 
-  
+
   # tf.profiler.profile(
       # tf.get_default_graph(), options=tf.profiler.ProfileOptionBuilder.float_operation())
   # return 
   # np.zero, np.one
-  feed2input = np.full((train_batch_size, n_steps * n_input), 0.)
+  feed2input = np.full((train_batch_size,  n_steps, n_input), 0.)
   
   # with tf.Session(config=tf.ConfigProto(log_device_placement=True)) as sess :
-  with tf.Session() as sess :
+  config = tf.ConfigProto()
+  config.gpu_options.allow_growth = True
+  with tf.Session(config = config) as sess:
     sess.run(tf.global_variables_initializer())
     
     # add additional options to trace the session execution
-    options = tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE)
-    run_metadata = tf.RunMetadata()
+    # options = tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE)
+    # run_metadata = tf.RunMetadata()
     
     # start = time.perf_counter()
     sess.run([output, h1], feed_dict={inputs: feed2input}, 
