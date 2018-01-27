767887be859292f7222dccc953ed0c203faf299e
HustCoderHu <coderhzx@gmail.com>
2018-01-27 23:53:29 +0800

    model save and restore -- and mobilenet_v2 mnist

--- TensorFlow/mnist/mnist_deep.py
+++ TensorFlow/mnist/mnist_deep.py
@@ -29,156 +29,120 @@ from __future__ import print_function
 import argparse
 import sys
 import tempfile
-
+import os
+import os.path as path
+import time
 from tensorflow.examples.tutorials.mnist import input_data
 
 import tensorflow as tf
 
+cwd = os.getcwd()
+sys.path.append(cwd)
+import mnist_model
+
 FLAGS = None
-data_dir = "MNIST_data/"
+data_dir = r"D:\github_repo\tftest\MNIST_data"
 
-def deepnn(x):
-  """deepnn builds the graph for a deep net for classifying digits.
-
-  Args:
-    x: an input tensor with the dimensions (N_examples, 784), where 784 is the
-    number of pixels in a standard MNIST image.
-
-  Returns:
-    A tuple (y, keep_prob). y is a tensor of shape (N_examples, 10), with values
-    equal to the logits of classifying the digit into one of 10 classes (the
-    digits 0-9). keep_prob is a scalar placeholder for the probability of
-    dropout.
-  """
-  # Reshape to use within a convolutional neural net.
-  # Last dimension is for "features" - there is only one here, since images are
-  # grayscale -- it would be 3 for an RGB image, 4 for RGBA, etc.
-  with tf.name_scope('reshape'):
-    x_image = tf.reshape(x, [-1, 28, 28, 1])
-
-  # First convolutional layer - maps one grayscale image to 32 feature maps.
-  with tf.name_scope('conv1'):
-    W_conv1 = weight_variable([5, 5, 1, 32])
-    b_conv1 = bias_variable([32])
-    h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
-
-  # Pooling layer - downsamples by 2X.
-  with tf.name_scope('pool1'):
-    h_pool1 = max_pool_2x2(h_conv1)
-
-  # Second convolutional layer -- maps 32 feature maps to 64.
-  with tf.name_scope('conv2'):
-    W_conv2 = weight_variable([5, 5, 32, 64])
-    b_conv2 = bias_variable([64])
-    h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
-
-  # Second pooling layer.
-  with tf.name_scope('pool2'):
-    h_pool2 = max_pool_2x2(h_conv2)
-
-  # Fully connected layer 1 -- after 2 round of downsampling, our 28x28 image
-  # is down to 7x7x64 feature maps -- maps this to 1024 features.
-  with tf.name_scope('fc1'):
-    W_fc1 = weight_variable([7 * 7 * 64, 1024])
-    b_fc1 = bias_variable([1024])
-
-    h_pool2_flat = tf.reshape(h_pool2, [-1, 7 * 7 * 64])
-    h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
-
-  # Dropout - controls the complexity of the model, prevents co-adaptation of
-  # features.
-  with tf.name_scope('dropout'):
-    keep_prob = tf.placeholder(tf.float32)
-    h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
-
-  # Map the 1024 features to 10 classes, one for each digit
-  with tf.name_scope('fc2'):
-    W_fc2 = weight_variable([1024, 10])
-    b_fc2 = bias_variable([10])
-
-    y_conv = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
-  return y_conv, keep_prob
-
-
-def conv2d(x, W):
-  """conv2d returns a 2d convolution layer with full stride."""
-  return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
-
-
-def max_pool_2x2(x):
-  """max_pool_2x2 downsamples a feature map by 2X."""
-  return tf.nn.max_pool(x, ksize=[1, 2, 2, 1],
-                        strides=[1, 2, 2, 1], padding='SAME')
-
-
-def weight_variable(shape):
-  """weight_variable generates a weight variable of a given shape."""
-  initial = tf.truncated_normal(shape, stddev=0.1)
-  return tf.Variable(initial)
-
-
-def bias_variable(shape):
-  """bias_variable generates a bias variable of a given shape."""
-  initial = tf.constant(0.1, shape=shape)
-  return tf.Variable(initial)
-
-
-def main(_):
+def main():
   # Import data
   mnist = input_data.read_data_sets(data_dir, one_hot=True, 
                                     source_url='http://yann.lecun.com/exdb/mnist/')
 
   # Create the model
-  x = tf.placeholder(tf.float32, [None, 784])
+  model = mnist_model.MobileNetV2_mnist()
+  return
 
   # Define loss and optimizer
   # y_ = tf.placeholder(tf.int64, [None])
   y_ = tf.placeholder(tf.float32, [None, 10])
 
   # Build the graph for the deep net
-  y_conv, keep_prob = deepnn(x)
 
   with tf.name_scope('loss'):
     # cross_entropy = tf.losses.sparse_softmax_cross_entropy(
         # labels=y_, logits=y_conv)
-    cross_entropy = tf.nn.softmax_cross_entropy_with_logits_v2(labels=y_, logits=y_conv)
-  cross_entropy = tf.reduce_mean(cross_entropy)
+    cross_entropy = tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=model.output)
+    # cross_entropy = tf.nn.softmax_cross_entropy_with_logits_v2(labels=y_, logits=y_conv)
+    cross_entropy = tf.reduce_mean(cross_entropy)
 
   with tf.name_scope('adam_optimizer'):
     train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
 
   with tf.name_scope('accuracy'):
-    correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1))
+    correct_prediction = tf.equal(tf.argmax(model.output, 1), tf.argmax(y_, 1))
     correct_prediction = tf.cast(correct_prediction, tf.float32)
-  accuracy = tf.reduce_mean(correct_prediction)
+    acc = tf.reduce_mean(correct_prediction, name="acc")
 
-  graph_location = tempfile.mkdtemp()
+  # print(cross_entropy.name) # loss/Mean:0
+  # print(accuracy.name) # accuracy/acc:0
+  # graph_location = tempfile.mkdtemp()
+  graph_location = r"D:\github_repo\tftest\mnist_deep_log"
   print('Saving graph to: %s' % graph_location)
   train_writer = tf.summary.FileWriter(graph_location)
   train_writer.add_graph(tf.get_default_graph())
 
+  # Add ops to save and restore all the variables.
+  saver = tf.train.Saver()
+
+  model_dir = r"D:\github_repo\tftest\mnist_deep_model"
+
   config = tf.ConfigProto()
-  config.gpu_options.allow_growth = True
-  
+  # config.gpu_options.allow_growth = True
+  config.gpu_options.per_process_gpu_memory_fraction = 0.75
+  # return
+  batch_size = 50
+  max_iter = 1 # int(55000 / batch_size * 1)
+  snapshot_intval = 500
+
+  save_path = "a"
   with tf.Session(config = config) as sess:
     sess.run(tf.global_variables_initializer())
-    for i in range(20000):
-      batch = mnist.train.next_batch(50)
+    for i in range(max_iter):
+      batch = mnist.train.next_batch(batch_size)
       if i % 100 == 0:
-        train_accuracy = accuracy.eval(feed_dict={
+        train_accuracy = acc.eval(feed_dict={
             x: batch[0], y_: batch[1], keep_prob: 1.0})
-        print('step %d, training accuracy %g' % (i, train_accuracy))
+        print('iter %d, training accuracy %g' % (i, train_accuracy))
       train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
+      if i % snapshot_intval == 0 and i > 0:
+        _prefix = "iter-{}".format(i)
+        ckpt_path = path.join(model_dir, _prefix)
+        save_path = saver.save(sess, ckpt_path)
+        print("snapshot to {}".format(save_path))
+    # train finish
+    _prefix = "iter-{}".format(max_iter - 1)
+    ckpt_path = path.join(model_dir, _prefix)
+    save_path = saver.save(sess, ckpt_path)
+    print("snapshot to {}".format(save_path))
     # error on my 750ti
     # Allocator (GPU_0_bfc) ran out of memory trying to allocate 957.03MiB
     # Resource exhausted: OOM when allocating tensor with shape[10000,32,28,28]
-    print('test accuracy %g' % accuracy.eval(feed_dict={
-        x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
+    # print('whole val set accuracy %g' % accuracy.eval(feed_dict={
+    #   x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
+  print("----------------------------")
+  print("save_path: {}".format(save_path))
+  print("begin test")
+  # test
+  # config.gpu_options.allow_growth = True
+  # tf.reset_default_graph()
+  print("----------------------------")
+  with tf.Session(config = config) as sess:
+    iter = max_iter - max_iter % snapshot_intval
+    _prefix = "iter-{}".format(max_iter - 1)
+    meta_path = path.join(model_dir, _prefix + ".meta")
+    # new_saver = tf.train.import_meta_graph(meta_path)
+    saver.restore(sess, save_path)
+
+    # with tf.variable_scope('accuracy', reuse=True) :
+    #   acc = tf.get_variable(name="acc")
+    print('whole val set accuracy %g' % accuracy.eval(feed_dict={
+      x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
 
 if __name__ == '__main__':
-  parser = argparse.ArgumentParser()
-  parser.add_argument('--data_dir', type=str,
-                      default='/tmp/tensorflow/mnist/input_data',
-                      help='Directory for storing input data')
-  FLAGS, unparsed = parser.parse_known_args()
-  tf.app.run(main=main, argv=[sys.argv[0]] + unparsed)
+  main()
+  # parser = argparse.ArgumentParser()
+  # parser.add_argument('--data_dir', type=str,
+  #                     default='/tmp/tensorflow/mnist/input_data',
+  #                     help='Directory for storing input data')
+  # FLAGS, unparsed = parser.parse_known_args()
+  # tf.app.run(main=main, argv=[sys.argv[0]] + unparsed)
---
+++ TensorFlow/mnist/mnist_model.py
@@ -0,0 +1,244 @@
+# Copyright 2015 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
+"""A deep MNIST classifier using convolutional layers.
+
+See extensive documentation at
+https://www.tensorflow.org/get_started/mnist/pros
+"""
+# Disable linter warnings to maintain consistency with tutorial.
+# pylint: disable=invalid-name
+# pylint: disable=g-bad-import-order
+
+# import sys
+# import tempfile
+# import os.path as path
+# import time
+
+import tensorflow as tf
+
+def main():
+  # Create the model
+  x = tf.placeholder(tf.float32, [None, 784])
+
+  # Define loss and optimizer
+  # y_ = tf.placeholder(tf.int64, [None])
+  y_ = tf.placeholder(tf.float32, [None, 10])
+
+  # y_conv, keep_prob = lenet(x)
+
+
+  return
+
+
+def lenet(x):
+  """deepnn builds the graph for a deep net for classifying digits.
+
+  Args:
+    x: an input tensor with the dimensions (N_examples, 784), where 784 is the
+    number of pixels in a standard MNIST image.
+
+  Returns:
+    A tuple (y, keep_prob). y is a tensor of shape (N_examples, 10), with values
+    equal to the logits of classifying the digit into one of 10 classes (the
+    digits 0-9). keep_prob is a scalar placeholder for the probability of
+    dropout.
+  """
+  # Reshape to use within a convolutional neural net.
+  # Last dimension is for "features" - there is only one here, since images are
+  # grayscale -- it would be 3 for an RGB image, 4 for RGBA, etc.
+  with tf.name_scope('reshape'):
+    x_image = tf.reshape(x, [-1, 1, 28, 28])
+    # print(x_image.shape)
+
+  # First convolutional layer - maps one grayscale image to 32 feature maps.
+  n_in = 1
+  n_out = 32
+  with tf.name_scope('conv1'):
+    W_conv1 = weight_variable([5, 5, n_in, n_out])
+    b_conv1 = bias_variable([n_out])
+    conv_res = conv2d(x_image, W_conv1)
+    conv_res = tf.nn.bias_add(conv_res, b_conv1, data_format="NCHW")
+    h_conv1 = tf.nn.relu(conv_res)
+
+  # Pooling layer - downsamples by 2X.
+  with tf.name_scope('pool1'):
+    h_pool1 = max_pool_2x2(h_conv1)
+
+  # Second convolutional layer -- maps 32 feature maps to 64.
+  n_in = n_out
+  n_out = 64
+  with tf.name_scope('conv2'):
+    W_conv2 = weight_variable([5, 5, n_in, n_out])
+    b_conv2 = bias_variable([n_out])
+    conv_res = conv2d(h_pool1, W_conv2)
+    conv_res = tf.nn.bias_add(conv_res, b_conv2, data_format="NCHW")
+    h_conv2 = tf.nn.relu(conv_res)
+
+  # Second pooling layer.
+  with tf.name_scope('pool2'):
+    h_pool2 = max_pool_2x2(h_conv2)
+
+  # Fully connected layer 1 -- after 2 round of downsampling, our 28x28 image
+  # is down to 7x7x64 feature maps -- maps this to 1024 features.
+  n_in = n_out
+  n_out = 1024
+  with tf.name_scope('fc1'):
+    h_pool2_flat = tf.reshape(h_pool2, [-1, 7 * 7 * n_in])
+    W_fc1 = weight_variable([n_in * 7 * 7, n_out])
+    # b_fc1 = bias_variable([n_out])
+    # print(W_fc1.shape)
+    # wfc1_shape = W_fc1.shape
+    h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1))
+
+  # Dropout - controls the complexity of the model, prevents co-adaptation of
+  # features.
+  with tf.name_scope('dropout'):
+    keep_prob = tf.placeholder(tf.float32)
+    h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
+
+  # Map the 1024 features to 10 classes, one for each digit
+  n_in = n_out
+  n_out = 10
+  with tf.name_scope('fc2'):
+    W_fc2 = weight_variable([n_in, n_out])
+    # b_fc2 = bias_variable([n_out])
+    y_conv = tf.matmul(h_fc1_drop, W_fc2)
+  return y_conv, keep_prob
+
+def conv2d(x, W):
+  """conv2d returns a 2d convolution layer with full stride."""
+  return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME',
+                      data_format="NCHW")
+
+def max_pool_2x2(x):
+  """max_pool_2x2 downsamples a feature map by 2X."""
+  return tf.nn.max_pool(x, ksize=[1, 1, 2, 2],
+                        strides=[1, 1, 2, 2], padding='SAME',
+                        data_format="NCHW")
+
+def weight_variable(shape):
+  """weight_variable generates a weight variable of a given shape."""
+  initial = tf.truncated_normal(shape, stddev=0.1)
+  return tf.Variable(initial)
+
+def bias_variable(shape):
+  """bias_variable generates a bias variable of a given shape."""
+  initial = tf.constant(0.1, shape=shape)
+  return tf.Variable(initial)
+
+class MobileNetV2_mnist():
+  def __init__(self, is_training=True, data_format="NCHW", input_size = 28):
+    self.is_training = is_training
+    self.data_format = data_format
+    self.input_size = input_size
+    # self.normalizer = tc.layers.batch_norm
+    self.bn_params = {'is_training': self.is_training}
+    with tf.variable_scope("mnist"):
+      self.x = tf.placeholder(dtype=tf.float32,
+                                  shape=[None, 1, input_size, input_size])
+      self.output = self._build_model(self.x)
+
+
+  def _build_model(self, x):
+    self.i = 0
+    with tf.variable_scope("init_conv") :
+      # w = tf.get_variable("first_w", )
+      n_not = 16
+      data_format = "channels_first" if self.data_format == "NCHW" \
+        else "channels_last "
+      output = tf.layers.conv2d(x, n_not, kernel_size=3, strides=1,
+                                padding="same", data_format=data_format,
+                                activation=None)
+      output = tf.contrib.layers.batch_norm(x, activation_fn=tf.nn.relu6,
+                                            is_training=self.is_training,
+                                            data_format=self.data_format)
+      output = self._inverted_bottleneck(output, 4, 32, subsample=False)
+      output = self._inverted_bottleneck(output, 4, 32, subsample=False)
+      output = self._inverted_bottleneck(output, 1, 10, subsample=True)
+      pool_size = output.shape[2:4] if self.data_format=="NCHW" \
+        else output.shape[1:3]
+      data_format = "channels_first" if self.data_format == "NCHW" \
+        else "channels_last "
+      output = tf.layers.average_pooling2d(output, pool_size, strides=None,
+                                           data_format=data_format)
+      print(output.shape)
+      return output
+
+
+  def _inverted_bottleneck(self, x, rate_channels, channels, subsample):
+    init = tf.contrib.layers.xavier_initializer(dtype=tf.float32)
+    in_shape = x.shape
+    n_in = in_shape[1] if self.data_format=="NCHW" else in_shape[-1]
+    print(type(n_in))
+    print(n_in)
+    stride = 2 if subsample else 1
+    with tf.name_scope("inverted_bottleneck_{}_{}_{}".format(
+      self.i, rate_channels, subsample ) ) :
+      # 1x1 pointwise
+      n_out = rate_channels * n_in
+      data_format = "channels_first" if self.data_format=="NCHW" \
+        else "channels_last "
+      output = tf.layers.conv2d(x, n_out, kernel_size=1, strides=stride,
+                       padding="same", data_format=data_format,
+                       activation=None, use_bias=False,
+                       kernel_initializer=init)
+      output = tf.contrib.layers.batch_norm(x, activation_fn=tf.nn.relu6,
+                                            is_training=self.is_training,
+                                            data_format=self.data_format)
+      # output = tf.layers.batch_normalization(x, )
+      # w1x1 = tf.get_variable("w1x1", [1, 1, n_in, n_out], initializer=init)
+      # b1x1 = tf.get_variable("b1x1", [n_out], initializer=init)
+      # output = tf.nn.conv2d(x, w1x1, strides=[1, 1, stride, stride],
+      #                       padding="SAME", data_format=self.data_format)
+      # 3x3 depthwise
+      n_in = n_out
+      w3x3 = tf.get_variable("w3x3", [3, 3, n_in, n_out], initializer=init)
+      # b3x3 = b1x1 = tf.get_variable("b3x3", [n_out], initializer=init)
+      output = tf.nn.depthwise_conv2d(x, w3x3, strides=[1, 1, stride, stride],
+                                      padding="same", data_format=self.data_format)
+      output = tf.contrib.layers.batch_norm(x, activation_fn=tf.nn.relu6,
+                                            is_training=self.is_training,
+                                            data_format=self.data_format)
+      # 1x1
+      n_out = channels
+      output = tf.layers.conv2d(x, n_out, kernel_size=1, strides=stride,
+                                padding="same", data_format=data_format,
+                                activation=None, use_bias=False,
+                                kernel_initializer=init)
+      # w1x1_1 = tf.get_variable("w1x1_1", [1, 1, n_in, n_out], initializer=init)
+      # b1x1_1 = tf.get_variable("b1x1_1", [n_out], initializer=init)
+      # output = tf.nn.conv2d(x, w1x1_1, strides=[1, 1, stride, stride],
+      #                       padding="SAME", data_format=self.data_format)
+      output = tf.contrib.layers.batch_norm(x, activation_fn=None,
+                                            is_training=self.is_training,
+                                            data_format=self.data_format)
+      output = tf.add(x, output)
+    self.i += 1
+    return output
+
+# end class MobileNetV2_mnist
+
+
+if __name__ == '__main__':
+  main()
+
+# reference
+# https://github.com/timctho/mobilenet-v2-tensorflow/blob/master/models.py
+# https://github.com/tonylins/pytorch-mobilenet-v2/blob/master/MobileNetV2.py
+
+# BN
+# http://blog.csdn.net/FontThrone/article/details/76652772
+# http://www.liulizhe.com/2017/09/12/cnn%E4%B8%AD%E7%9A%84batch-normalization%E5%8F%8A%E5%85%B6tensorflow%E7%94%A8%E6%B3%95/
