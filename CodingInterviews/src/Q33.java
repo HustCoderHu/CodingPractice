bab036bab12b51ad402e80ad2723b534ca5efd90
HustCoderHu <coderhzx@gmail.com>
2018-04-11 21:17:25 +0800

    剑指 33

---
+++ CodingInterviews/src/Q33.java
@@ -0,0 +1,51 @@
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Q33 {
+//  final int INT_MAXLEN = 10;
+//  char[] str1 = new char[INT_MAXLEN * 2 + 1];
+//  char[] str2 = new char[INT_MAXLEN * 2 + 1];
+
+  public static void main(String[] args) {
+    int[] numbers = {3, 32, 321};
+    System.out.println(PrintMinNumber(numbers));
+  }
+
+  public static String PrintMinNumber(int [] numbers) {
+    final int nNum = numbers.length;
+    String[] strNums = new String[nNum];
+
+    for (int i = 0; i < nNum; i++) {
+      strNums[i] = String.valueOf(numbers[i]);
+    }
+
+    Arrays.sort(strNums);
+    Arrays.sort
+
+    StringBuilder str = new StringBuilder();
+    for (String s : strNums)
+      str.append(s);
+
+    return str.toString();
+  }
+}
+
+class MyComp implements Comparator<String> {
+  static final int INT_MAXLEN = 10;
+  static StringBuilder stra = new StringBuilder(INT_MAXLEN * 2 + 1);
+  static StringBuilder strb = new StringBuilder(INT_MAXLEN * 2 + 1);
+
+  @Override
+  public int compare(java.lang.String o1, java.lang.String o2) {
+    stra.delete(0, stra.length());
+    strb.delete(0, strb.length());
+
+    stra.append(o1);
+    stra.append(o2);
+    strb.append(o2);
+    strb.append(o1);
+
+    stra.toString().equals(strb.toString());
+    return 0;
+  }
+}
